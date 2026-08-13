f9322a1540556e740b72549f487f827f8cd769c3
Rajveer Jadhav <jadhavrajveer86@gmail.com>
2026-08-13 23:44:33 +0530

    Time: 26 ms (27.46%) | Memory: 13.4 MB (40.45%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
         if len(s)!=len(t):
             return False
         
