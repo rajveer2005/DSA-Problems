7340e5547e3cecf85828c07564caa1cc949be3b8
Rajveer Jadhav <jadhavrajveer86@gmail.com>
2026-08-22 22:17:38 +0530

    Time: 2122 ms (33.77%) | Memory: 13.3 MB (36.56%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -6,9 +6,9 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(nums)
-
         for i in range(0,n-1):
             for j in range(i+1,n):
                 if nums[i] + nums[j] == target:
                     return [i,j]
+            
         
