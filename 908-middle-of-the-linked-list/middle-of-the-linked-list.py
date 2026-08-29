5400f8a72036c4e7acb317b71693ffa443c98707
Rajveer Jadhav <jadhavrajveer86@gmail.com>
2026-08-29 23:57:18 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (91.67%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -9,14 +9,22 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        n = 0
-        temp = head
-        while temp :
-            n +=1
-            temp = temp.next
-        temp = head            
-        for i in range(0,n//2):
-            temp = temp.next
-        return temp
+        # n = 0
+        # temp = head
+        # while temp :
+        #     n +=1
+        #     temp = temp.next
+        # temp = head            
+        # for i in range(0,n//2):
+        #     temp = temp.next
+        # return temp
+
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
 
         
