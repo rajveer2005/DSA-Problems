5d255c79b3e3e0400b6858598dd1b78dc8b9a96a
Rajveer Jadhav <jadhavrajveer86@gmail.com>
2026-09-04 18:11:54 +0530

    Time: 18 ms (99.38%) | Memory: 18.6 MB (90.12%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -10,12 +10,19 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        temp = head
-        my_set =  set()
-        while temp is not None:
-            if temp in my_set:
-                return temp
-            my_set.add(temp)
-            temp = temp.next
+        slow = head
+        fast = head
+        
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
         return None
+
+            
         
